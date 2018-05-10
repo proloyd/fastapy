d90daefc145a1063efa50e3e0fc9f1cc2d9f4544
Proloy Das <proloy@umd.edu>
2018-05-10 18:25:55 -0400

    class implementation

--- fasta.py
+++ fasta.py
@@ -1,135 +1,295 @@
 """
-Module implementing FASTA:
-
-An fast implementation of Proximal Gradient Descent solver
-Created on Sun Mar 11 16:05:29 2018
-
-@author: Proloy Das
-
-@licence = apache 2.0
+Module implementing the FASTA algorithm
 """
 
+__author__ = 'Proloy DAS'
+__licence__ = 'apache 2.0'
+
 import numpy as np
-import tqdm
-import time
 from scipy import linalg
+import time
 
 
-def fastapy(f, g, gradf, proxg, x0, beta=0.5, max_Iter=1000, tol=1e-8):
-     """
-     Solves
-                        \min_{x} f(x) + g(x)
-     given following inputs:
+def _next_stepsize( deltax, deltaF ):
+    """
 
-    :param f: function handle of smooth differentiable function,
-    :math:                        $f(x)$
+    A variation of spectral descent step-size selection: 'adaptive' BB method.
 
-    :param g: function handle of non-smooth convex function, g(x)
-    :math:                        $g(x)$
+    Reference:
+    ---------
+    B. Zhou, L. Gao, and Y.H. Dai, 'Gradient methods with adaptive step-sizes,'
+    Comput. Optim. Appl., vol. 35, pp. 69-86, Sept. 2006
 
-    :param gradf: function handle for gradient of smooth differentiable function
-    :math:                     $\nabla f(x)$
+    parameters
+    ----------
+    deltax: ndarray
+        difference between coefs_current and coefs_next
 
-    :param proxg: function handle for proximal operator of non-smooth convex function
-    :math: ${\sf prox}_{\lambda g}(v) = \argmin_{x} g(x) + 1/(2*lambda)\| x-v\|**2
+    deltaF: ndarray
+        difference between grad operator evaluated at coefs_current and coefs_next
 
-    :param x0: initial guess
-            (M,) 1D array or (M,M) 2D array
+    returns
+    -------
+    float
+    adaptive step-size
 
-    :param beta: backtracking parameter
-            scalar float
-            uses 0.5 by default.
-    :param max_Iter: maximum number of iterations
-            scalar integer
-            uses 1000 by default
-    :param tol: tolerance
-            scalar float
-            uses 1e-8 as default
+    """
+    n_deltax = linalg.norm (deltax, 'fro') ** 2
+    n_deltaF = linalg.norm (deltaF, 'fro') ** 2
+    innerproduct_xF = np.real(np.sum (deltax * deltaF))
 
-    :return: a dict containing the
-    "solution": (M,) 1D array or (M,M) 2D array
-    "objective values": 1D array
-    "residuals": 1D array
+    if n_deltax == 0:
+        return 0
+    elif (n_deltaF == 0) | (innerproduct_xF == 0):
+        return -1
+    else:
+        tau_s = n_deltax / innerproduct_xF  # steepest descent
+        tau_m = innerproduct_xF / n_deltaF  # minimum residual
 
-    Caution:
+        # adaptive BB method
+        if 2 * tau_m > tau_s:
+            return tau_m
+        else:
+            return tau_s - 0.5 * tau_m
+
+
+def _compute_residual( deltax, deltaF, tau ):
+    """
+
+    Compute Residual at given step
+
+   parameters
+    ----------
+    deltax: ndarray
+        difference between coefs_current and coefs_next
+
+    deltaF: ndarray
+        difference between grad operator evaluated at coefs_current and coefs_next
+
+    tau: float
+        current stepzise
+
+    returns
+    -------
+    float
+    residual at current step
+    """
+    return linalg.norm (deltaF - deltax / tau, 'fro') ** 2
+
+
+def _update_coefs(x, tau, gradfx, prox, f, beta, fk):
+    """
+
+    Non-monotone line search
+
+    parameters
+    ----------
+    x: ndarray
+        current coefficients
+
+    tau: float
+        step size
+
+    gradfx: ndarry
+        gradient operator evaluated at current coefficients
+
+    prox: function handle
+        proximal operator of :math:`g(x)`
+
+    f: function handle
+        smooth differentiable function, :math:`f(x)`
+
+    beta: float
+        backtracking parameter
+
+    fk: float
+        maximum of previous function values
+
+    returns
+    -------
+    z: ndarray
+        next coefficients
+
+    """
+    z = prox (x - tau * gradfx, tau)
+    fz =  f(z)
+    count = 0
+    while fz > fk + np.sum (gradfx * (z - x)) + np.square (linalg.norm (z - x, 'fro')) / (2 * tau):
+        count += 1
+        tau = beta * tau
+        z = prox (x - tau * gradfx, tau)
+        fz = f(z)
+
+    return z, fz, tau, count
+
+
+class Fasta:
+    """
+
+    Fast adaptive shrinkage/thresholding Algorithm
+
+    Parameters
+    ----------
+    f: function handle
+        smooth differentiable function, :math:`f(x)`
+
+    g: function handle
+        non-smooth convex function, :math:`g(x)`
+
+    gradf: function handle
+        gradient of smooth differentiable function, :math:`\\nabla f(x)`
+
+    proxg: function handle
+        proximal operator of non-smooth convex function
+     :math:`proxg(v, \\lambda) = argmin g(x) + \\frac{1}{2*\\lambda}\|x-v\|^2`
+
+    beta: float, optional
+        backtracking parameter
+        default is 0.5
+
+    n_iter: int, optional
+        number of iterations
+        default is 1000
+
+    Attributes
+    ----------
+    coefs: ndvar
+        learned coefficients
+
+    objective_value: float
+        optimum objective value
+
+    residuals: list
+        residual values at each iteration
+
+    initial_stepsize: float, optional
+        created only with verbose=1 option
+
+    objective: list, optional
+        objective values at each iteration
+        created only with verbose=1 option
+
+    stepsizes: list, optional
+        stepsizes at each iteration
+        created only with verbose=1 option
+
+    backtracks: list, optional
+        number of backtracking steps
+        created only with verbose=1 option
+    Notes
+    -----
     Make sure that outputs of gradf and proxg is of same size as x.
     The implementation does not check for any such discrepancies.
 
-    Usage: Solve following least square problem using fastapy
-                min  .5||Ax-b||^2 + mu*|x|
-    # Create function handles
+    Use
+    ---
+    Solve following least square problem using fastapy
+                :math:`\\min  .5||Ax-b||^2 + \\mu*\|x\|_1`
+    Create function handles
     >>> def f(x): return 0.5 * linalg.norm(np.dot(A, x) - b, 2)**2  # f(x) = .5||Ax-b||^2
     >>> def gradf(x): return np.dot(A.T, np.dot(A, x) - b)  # gradient of f(x)
     >>> def g(x): return mu * linalg.norm(x, 1)  # mu|x|
     >>> def proxg(x, t): return shrink(x, mu*t)
     >>> def shrink(x, mu): return np.multiply(np.sign(x), np.maximum(np.abs(x) - mu, 0)) #proxg(z,t) = sign(x)*max(|x|-mu,0)
-    # Call Solver
-    >>> out = fastapy( f, g, gradf, proxg, x0, tol=1e-20)
+    Create FASTA instance
+    >>> lsq = Fasta(f, g, gradf, proxg)
+    Call solver
+    >>> lsq.learn(x0, verbose=1)
 
     """
-    
-        # estimate Lipschitz constant ans initialize tau
-    x = x0 + 0.01 * np.random.randn(x0.shape[0], x0.shape[1])
-    L = np.square(linalg.norm(gradf(x0) - gradf(x), 'fro')) / np.square(linalg.norm(x0 - x, 'fro'))
-    tau = 1 / L
 
-    x = np.copy(x0)
-    gradfx = gradf(x)
+    def __init__(self, f, g, gradf, proxg, beta=0.5, n_iter=1000):
+        self.f = f
+        self.g = g
+        self.grad = gradf
+        self.prox = proxg
+        self.beta = beta
+        self.n_iter = n_iter
+        self.residuals = []
+        self._funcValues = []
 
-    # Save f(x) values for back-tracking
+    def __str__(self):
+        return "Fast adaptive shrinkage/thresholding Algorithm instance"
 
-    fx = np.empty((0))
-    fx = np.append(fx, f(x))
+    def learn(self, coefs_init, tol=1e-8, verbose=0):
+        """
 
-    # Save objective values for returing
-    fval = np.empty((0))
-    fval = np.append(fval, fx[-1] + g(x))
+        parameters
+        ----------
+        coefs_init: ndarray
+            initial guess
 
-    # Save Residuals for returning
-    residual = np.empty((0))
+        tol: float, optional
+            tolerance parameter
+            default is 1e-8
 
-    for _ in tqdm.tqdm(range(max_Iter)):
-        time.sleep(0.001)
+        verbose: {0,1}
+            verbosity of the method : 1 will display informations while 0 will display nothing
+            default = 0
 
-        # backtracking for step size - tau
-        z = proxg(x - tau * gradfx, tau)
-        fk = np.max(fx)
-        # fk = fx[-1]
-        while f(z) > fk + np.sum(gradfx * (z - x)) + np.square(linalg.norm(z - x, 'fro')) / (2 * tau):
-            tau = beta * tau
-            z = proxg(x - tau * gradfx, tau)
+        returns
+        -------
+        self
+        """
+        coefs_current = np.copy(coefs_init)
+        grad_current = self.grad (coefs_current)
+        coefs_next = coefs_current \
+                     + 0.01 * np.random.randn(coefs_current.shape[0], coefs_current.shape[1])
+        grad_next = self.grad(coefs_next)
+        tau_current = _next_stepsize(coefs_next - coefs_current,
+                             grad_next - grad_current)
 
-        # Check for convergence and if reached break
-        gradfz = gradf(z)
-        residual = np.append(residual, linalg.norm(gradfz + (x - tau * gradfx - z) / tau, 'fro') ** 2)
-        if residual[-1] / residual[0] < tol:
-            break
+        self._funcValues.append (self.f (coefs_current))
+        if verbose == 1:
+            self.objective = []
+            self.objective.append(self._funcValues[-1] + self.g(coefs_current))
+            self.initial_stepsize = np.copy(tau_current)
+            self.stepsizes = []
+            self.backtracks = []
 
-        # choose next step size using adaptive BB method
-        deltax = z - x
-        deltaF = gradfz - gradfx
-        n_deltax = linalg.norm(deltax, 'fro') ** 2
-        n_deltaF = linalg.norm(deltaF, 'fro') ** 2
-        innerproduct_xF = np.sum(deltax * deltaF)
-        if n_deltax == 0:
-            break
-        elif (n_deltaF == 0) | (innerproduct_xF == 0):
-            tau = 1 / L
-        else:
-            tau_s = n_deltax / innerproduct_xF  # steepest descent
-            tau_m = innerproduct_xF / n_deltaF  # minimum residual
-            # adaptive BB method
-            if 2 * tau_m > tau_s:
-                tau_k = tau_m
+        start = time.time()
+        for i in range(self.n_iter):
+            coefs_next, objective_next, tau, n_backtracks \
+                = _update_coefs(coefs_current, tau_current, grad_current,
+                                self.prox, self.f, self.beta, max(self._funcValues))
+
+            self._funcValues.append(objective_next)
+
+            grad_next = self.grad(coefs_next)
+
+            # Find residual
+            delta_coef = coefs_current - coefs_next
+            delta_grad = grad_current - grad_next
+            residual = _compute_residual(delta_coef, delta_grad, tau)
+            self.residuals.append(residual)
+
+            # Find step size for next iteration
+            tau_next = _next_stepsize(delta_coef, delta_grad)
+
+            if verbose == 1:
+                self.stepsizes.append(tau)
+                self.backtracks.append(n_backtracks)
+                self.objective.append(objective_next + self.g(coefs_next))
+                print("Iteration : {:}, objective value : {:f}, stepsize : {:f},"
+                      " backtracking steps taken: {:}, residual : {:f} \n"
+                      .format(i+1, self.objective[i], self.stepsizes[i]
+                              , self.backtracks[i], self.residuals[i]))
+
+            # Prepare for next iteration
+            coefs_current = coefs_next
+            grad_current = grad_next
+
+            if tau_next == 0 or residual < tol:    # convergence reached
+                break
+            elif tau_next < 0:  # non-convex probelms ->  negative stepsize -> use the previous value
+                tau_current = tau
             else:
-                tau_k = tau_s - 0.5 * tau_m
-            if tau_k > 0:
-                tau = tau_k
+                tau_current = tau_next
 
-        x = np.copy(z)
-        gradfx = np.copy(gradfz)
-        fx = np.append(fx, f(x))
-        fval = np.append(fval, fx[-1] + g(x))
+        end = time.time()
+        self.coefs_ = coefs_current
+        self.objective_value = objective_next + self.g(coefs_current)
+        if verbose:
+            print "total time elapsed : {:f}s".format(end - start)
 
-    out = {"sol": x, "objective values": fval, "residual": residual}
-    return out
+        return self
