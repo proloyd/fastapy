75a0510517f212095c14033c6331bca55fe0c83f
AD\proloy <proloy@umd.edu>
2018-06-07 17:34:21 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,20 @@ from setuptools import setup, find_packages
 setup(
     name="fasta",
     version="0.1",
+    description="fast adaptive proximal gradient descent algorithm",
     packages=find_packages(),
+
+    install_requires=[
+        'numpy >= 1.8',
+        'scipy' >=0.17,
+    ],
+
+
+    # metadata for upload to PyPI
+    author = "Proloy Das",
+    author_email = "proloy@umd.com",
+    license = "apache",
+    project_urls = {
+        "Source Code": "https://github.com/proloyd/fastapy",
+    }
 )
